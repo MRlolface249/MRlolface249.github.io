e299ecc57541dd17015d44743ad65075ca3ed6ea
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-03-15 00:11:11 +1300

    created multiple pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import NoPage from "./components/NoPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="aboutMe" element={<AboutMe />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "../style.css";
+import jester from "../images/Hasidu_Jester.jpg";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+        <div class="menuBar">
+          <Link to="/"> Home </Link>
+          <Link to="/aboutMe"> About Me </Link>
+          <Link to="/projects"> Projects </Link>
+        </div>
+        <div class="photo">
+          <img src={jester} alt="jester" />
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
